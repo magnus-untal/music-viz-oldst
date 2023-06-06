acf3ced185efa19acec2909d6b9c73b94702f6f2
Kirsten Magnus T. Untal <kirsten.untal@obf.ateneo.edu>
2023-06-06 22:36:47 +0800

    initial commit

---
+++ Music Visualization/Assets/Scripts/AudioPeer.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent (typeof (AudioSource))]
+public class AudioPeer : MonoBehaviour
+{
+
+    AudioSource _audioSource;
+    public static float[] _samples = new float[512];
+    public static float[] _freqBand = new float[8];
+    public static float[] _bandBuffer = new float[8];
+    float[] _bufferDecrease = new float[8];
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetSpectrumAudioSource();
+        MakeFrequencyBands();
+        BandBuffer();
+    }
+
+    // GetSpectrumData
+
+    void GetSpectrumAudioSource() {
+        _audioSource.GetSpectrumData(_samples,0,FFTWindow.Blackman);
+    }
+
+    void BandBuffer() {
+        for(int i = 0; i < 8; i++) {
+            if (_freqBand[i] > _bandBuffer[i]) {
+                _bandBuffer[i] = _freqBand[i];
+                _bufferDecrease[i] = 0.005f;
+            }
+            if(_freqBand[i] < _bandBuffer[i]) {
+                _bandBuffer[i] -= _bufferDecrease[i];
+                _bufferDecrease[i] *= 1.02f;
+            }
+        }
+    }
+
+    void MakeFrequencyBands() {
+        /* 
+        44100hz track over 2 channels
+        ~22050hz loading in
+
+        22050/512 = ~43hz per sample
+        Frequency Bands
+        20-60
+        60-250
+        250-500
+        500-2000
+        2000-4000
+        4000-6000
+        6000-20000
+
+        we set up our frequency bands in powers of 2 based on our samples to get close to the table above
+        band 0 = 2 samples (0-86hz)
+        band 1 = 4 samples (87-258hz)
+        band 2 = 8 samples (259-602hz)
+        band 3 = 16 samples (603-1290hz)
+        band 4 = 32 samples (1291-2666hz)
+        band 5 = 64 samples (2667-5418hz)
+        band 6 = 128 samples (5419-10922hz)
+        band 7 = 256 samples + 2 remaining = 258 samples (10923-22050hz)
+        */
+        //we create a nested for loop
+        //first for loop to put numbers into our frequency bands
+        //second for loop to get the samples for our computations
+
+        int count = 0; //counts our current position on the samples
+        for(int i = 0; i<8; i++){
+            int sampleCount = (int) Mathf.Pow(2, i + 1); // based on our band calculations it is 2^(band number + 1)
+            float average = 0; //average of data which gets stored in our frequency bands
+            if(i==7){
+                sampleCount += 2;
+            }
+            for(int j = 0; j<sampleCount; j++){
+                average += _samples [count]  * (count + 1);
+                count++;
+            }
+
+            average /= count;
+
+            _freqBand[i] = average * 10;
+        }
+    }
+}
---
+++ Music Visualization/Assets/Scripts/InstantiateCubes.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateCubes : MonoBehaviour
+{
+    public GameObject _sampleCubePrefab;
+    GameObject[] _sampleCubes = new GameObject[512];
+    public float _maxScale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i< 512; i++){
+            GameObject _instanceSampleCube = (GameObject)Instantiate (_sampleCubePrefab);
+            _instanceSampleCube.transform.position = this.transform.position;// sit at center of spawn
+            _instanceSampleCube.transform.parent = this.transform; // set the parent of the cube to be the instantiate cube object
+            _instanceSampleCube.name = "SampleCube" + i; // rename to make it easier to track our cubes
+            this.transform.eulerAngles = new Vector3 (0, -0.703125f * i, 0); // rotate our object
+            _instanceSampleCube.transform.position = Vector3.forward * 500; // move cube forward 100 units (forms the circle)
+            _sampleCubes [i] = _instanceSampleCube;
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i< 512; i++){
+            if(_sampleCubes != null) {
+                // Every frame we will do a local scale on our cubes (we modify values of the cubes individually)
+                // we keep the x and z (width and depth) of the object to be a static 10 units
+                // we scale the y (height) based on the value given to us by the audio sample
+                _sampleCubes[i].transform.localScale = new Vector3(5, (AudioPeer._samples[i] * _maxScale) + 2, 5);
+
+            }
+        }   
+    }
+}
